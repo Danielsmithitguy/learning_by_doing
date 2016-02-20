c006d82d7b533a0ed99090f3a9217514b65e767e
Daniel Smith <Daniel.Smith.ITguy@gmail.com>
2016-02-19 17:10:43 -0700

    forgot var for puts baised on current age input
    [#114106375]

--- numbers_letters_and_variables/How_old.rb
+++ numbers_letters_and_variables/How_old.rb
@@ -29,7 +29,8 @@ puts "There are #{days_in_week} days in a week"
 puts "  #{seconds_in_min * min_in_hour} seconds in an hour,"
 puts "  #{seconds_in_min * min_in_hour * hours_in_day} seconds in an day,"
 puts "  #{seconds_in_min * min_in_hour * hours_in_day * days_in_week} seconds in a week"
-puts "That means when you turn 20, you've been alive for #{((seconds_in_min * min_in_hour * hours_in_day) * 365) * 20} seconds"
+puts "That means at age #{current_age}, you've been alive for #{((seconds_in_min * min_in_hour * hours_in_day) * 365) * current_age.to_i} seconds"
+puts "That also means when you turn 20, you've been alive for #{((seconds_in_min * min_in_hour * hours_in_day) * 365) * 20} seconds"
 puts "#and if you make it to 100, you will have lived #{((seconds_in_min * min_in_hour * hours_in_day) * 365) * 100} seconds. Make them count!'"
 
 if year % 4 == 0
