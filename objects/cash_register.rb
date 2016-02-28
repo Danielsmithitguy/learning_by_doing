05bdaccbba1fd1b3554c677f3f9011ee5f127f11
Daniel Smith <Daniel.Smith.ITguy@gmail.com>
2016-02-27 17:29:21 -0700

    Uploading cash_register.rb, still unresolved issue with Int sanataion, will still defalut anything *. as false.
    [#114623533]

---
+++ objects/cash_register.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+
+#done
+#This program should take a user input and either add or subtract from a running total.
+#if user is paying off a running total and it goes bellow 0 it should give change back
+#Should also output the current running total with each line
+#user should only be allowed to input choice inputs and nothing outside of that
+
+class CashRegister
+  def initialize
+    @currently_adding = true
+    @running_total = 0.0
+  end
+
+  def get_user_input
+    gets.chomp
+  end
+
+  def user_output
+    if @running_total > 0
+    puts "Your current total is #{@running_total} dollars"
+    end
+  end
+
+  def user_input(input)
+    #should get user input and either add, subtract or exit the program
+    #input can either be quit, checkout, add more items, a valid int, or not a valid int
+    if input == "quit" || input == 'q' || input == 'Q'
+      if @running_total != 0
+        puts "I can't exit right now, you still owe me #{@running_total} dollars! D:"
+      else
+        puts "Good bye!"
+        exit
+      end
+      #@currently_adding tells me if im checking out (false)
+      #or currently paying (true) my total.
+    elsif input == 'add more' || input == 'a' || input == 'A'
+      @currently_adding = true
+    elsif input == 'checkout' || input == 'c' || input == 'C'
+      @currently_adding = false
+    elsif is_valid_int(input)
+      #input is valid int, gsub used incase of type for , converting it to a . insted
+      if @currently_adding
+        #running total should increase with each input
+        @running_total = @running_total + input.to_f
+      else
+        #running total should decrease with each input, paying the total.
+        #should also not go below 0
+        @running_total = @running_total - input.to_f
+        if @running_total <= 0
+          puts "Your change is #{@running_total.abs}."
+          puts 'You may now leave the terminal by typing (Q)uit or feel free to add more things!'
+          initialize
+        end
+      end
+    else
+      #not a valid input
+      puts "Not a valid number, did you want to  (C)heckout? or (A)dd more or (Q)uit?"
+    end
+  end
+end
+
+def is_valid_int(input)
+  #basic int sanitizer
+  if input.include? "."
+  else
+    input = input + ".0"
+  end
+  if input.to_s == input.to_f.to_s
+    return true
+  else
+    return false
+  end
+end
+
+
+x = CashRegister.new
+puts "Welcome! feel free to start adding prices to your cart!" \
+         "\nwhen your finished just type (C)heckout or (Q)uit to exit."
+while true
+  x.user_output
+  x.user_input(x.get_user_input)
+end
