1e81734ee0797ace15f0109f1d5eb620598b08f3
Daniel Smith <Daniel.Smith.ITguy@gmail.com>
2016-03-16 14:54:30 -0600

    fixed code so that it was called recrusvly.
    [#114449935]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,34 +1,27 @@
 #!/usr/bin/env ruby
 
-def count(x)
-  if x >= 1
-    return x
-  elsif
-    x < 1
-    return 0
-  end
-end
 
-starting_amount = 100
-while starting_amount >= 0
-  if starting_amount >= 3
-    puts "#{count(starting_amount)} bottles of beer on the wall, #{count(starting_amount)} bottles of beer."
-    puts "Take one down and pass it around, #{count(starting_amount)- 1} bottles of beer on the wall."
+def bottles_of_beer (count)
+  if count >= 3
+    puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
+    puts "Take one down and pass it around, #{count- 1} bottles of beer on the wall."
     puts ""
-  elsif starting_amount == 2
-    puts "#{count(starting_amount)} bottles of beer on the wall, #{count(starting_amount)} bottles of beer."
-    puts "Take one down and pass it around, #{count(starting_amount)- 1} bottle of beer on the wall."
+  elsif count == 2
+    puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
+    puts "Take one down and pass it around, #{count - 1} bottle of beer on the wall."
     puts ""
-  elsif starting_amount == 1
-    puts "#{count(starting_amount)} bottles of beer on the wall, #{count(starting_amount)} bottles of beer."
+  elsif count == 1
+    puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
     puts "Take one down and pass it around, no more bottles of beer on the wall."
     puts ""
-  else
-    puts 'No more bottles of beer on the wall, no more bottles of beer.'
-    puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+  elsif
+    puts "No more bottles of beer on the wall, no more bottles of beer.\n" \
+     'Go to the store and buy some more, 99 bottles of beer on the wall.'
     puts ""
+  else
+    return 0
   end
-  starting_amount -= 1
+  bottles_of_beer(count-1)
 end
 
-
+bottles_of_beer(100)
