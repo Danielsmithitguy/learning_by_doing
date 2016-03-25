f91787af997371da418a637818a80b627f99e97b
Daniel Smith <Daniel.Smith.ITguy@gmail.com>
2016-03-25 14:34:27 -0600

    refactored and fixed more of the int santation, still has issues with anything ending with '0', ie .50 vs .5.

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -17,7 +17,20 @@ class CashRegister
 
   def user_output
     if @running_total > 0
-      puts "Your current total is #{@running_total} dollars"
+      puts "Your current total is #{format_running_total} dollars"
+    end
+  end
+
+  def format_running_total
+    sprintf('%.2f', @running_total)
+  end
+
+  def quit
+    if @running_total != 0
+      puts "I can't exit right now, you still owe me #{format_running_total} dollars! D:"
+    else
+      puts 'Good bye!'
+      exit
     end
   end
 
@@ -25,12 +38,7 @@ class CashRegister
     # should get user input and either add, subtract or exit the program
     # input can either be quit, checkout, add more items, a valid int, or not a valid int
     if input == 'quit' || input == 'q' || input == 'Q'
-      if @running_total != 0
-        puts "I can't exit right now, you still owe me #{@running_total} dollars! D:"
-      else
-        puts 'Good bye!'
-        exit
-      end
+      quit
       # @currently_adding tells me if im checking out (false)
       # or currently paying (true) my total.
     elsif input == 'add more' || input == 'a' || input == 'A'
@@ -41,12 +49,12 @@ class CashRegister
       # input is valid int, gsub used incase of type for , converting it to a . insted
       if @currently_adding
         # running total should increase with each input
-        @running_total += input.to_f
+        Adding_to_total(input)
       else
         # running total should decrease with each input, paying the total.
         # should also not go below 0 if payment causes total to go below 0
         # output change
-        @running_total -= input.to_f
+        Subtract_from_total(input)
         if @running_total <= 0
           puts "Your change is #{@running_total.abs}."
           puts 'You may now leave the terminal by typing (Q)uit or feel free to add more things!'
@@ -57,6 +65,14 @@ class CashRegister
       # not a valid input
       puts 'Not a valid number, did you want to  (C)heckout? or (A)dd more or (Q)uit?'
     end
+  end
+
+  def Subtract_from_total(input)
+    @running_total -= input.to_f
+  end
+
+  def Adding_to_total(input)
+    @running_total += input.to_f
   end
 end
 
@@ -71,6 +87,11 @@ def valid_int?(input)
     # checks to see if the input ends with . ex 3. would return false with out this
     input += '0'
   end
+   # checkes for instances of value starting with . ie .5 or something lower then 1.
+  if input.start_with?('.')
+    input = '0' + input.to_s
+  end
+  puts input.to_s + " | #{input.to_f}"
   if input.to_s == input.to_f.to_s
     return true
   else
